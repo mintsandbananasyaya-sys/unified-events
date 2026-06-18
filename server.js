7b3836c18adb84b2bb099a92d3bafe8093a5a4e3
mintsandbananasyaya-sys <mintsandbananasyaya@gmail.com>
2026-06-18 02:24:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -110,6 +110,6 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Running on http://localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
